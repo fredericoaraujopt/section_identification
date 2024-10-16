22ea210ba811fbf30c91afe39f68a4632dbcc8c0
Thomas Athey <tom.l.athey@gmail.com>
2024-10-16 14:43:09 -0400

    skeleton

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+__version__ = "0.0.1"
+
+setup(
+    name="section_identification",
+    version=__version__,
+    packages=["section_identification"],
+    author="*********",
+    author_email="*********",
+    install_requires=[
+        "numpy",
+    ]
+)
